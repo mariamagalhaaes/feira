53ba24e7450f583455184047ef8f9b03f340ed38
Maria <mariaeduardasilvamagalhes2729@gmail.com>
2025-05-29 15:11:17 -0300

    final

--- php/contato.php
+++ php/contato.php
@@ -2,9 +2,9 @@
 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nome = htmlspecialchars($_POST["nome"] ?? '');
-    $email = htmlspecialchars($_POST["email"] ?? '');
-    $mensagem = nl2br(htmlspecialchars($_POST["mensagem"] ?? ''));
+    $nome = htmlspecialchars($_POST["nome"]  );
+    $email = htmlspecialchars($_POST["email"]  );
+    $mensagem = nl2br(htmlspecialchars($_POST["mensagem"]  ));
 
     echo "<h2>Mensagem recebida com sucesso!</h2>";
 }
